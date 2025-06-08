197561b59aca68cc9dd789eafdaafc877e79f611
nadellasaideep1 <64152721+nadellasaideep1@users.noreply.github.com>
2025-06-08 13:12:38 +0530

    Update UNHRC.jsx

--- src/pages/committees/UNHRC.jsx
+++ src/pages/committees/UNHRC.jsx
@@ -19,7 +19,7 @@ function UNHRC() {
         backgroundBlendMode: "overlay",
       }}
     >
-      <div className="max-w-100% mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-centre">
+      <div className="max-w-100% mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       <h1 className="text-6xl md:text-9xl font-black tracking-tight text-center mt-8 md:mt-16 mb-8 break-words">
   United Nations<br className="inline md:hidden" />Human Rights Council
       </h1>
